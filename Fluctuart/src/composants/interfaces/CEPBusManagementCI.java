ef5ba7b2ebd7824d65bfdd5919008c2b9781e4c3
28705621 <28705621@ppti-14-406-03.ufr-info-p6.jussieu.fr>
2022-02-07 16:47:08 +0100

    busManagement

--- Fluctuart/src/composants/interfaces/CEPBusManagementCI.java
+++ Fluctuart/src/composants/interfaces/CEPBusManagementCI.java
@@ -4,5 +4,15 @@ import fr.sorbonne_u.components.interfaces.OfferedCI;
 import fr.sorbonne_u.components.interfaces.RequiredCI;
 
 public interface CEPBusManagementCI extends RequiredCI, OfferedCI {
+	
+	public String registerEmitter(String uri);
+	public void unregisterEmitter(String uri);
+	public String registerCorrelator(String uri, String inboundPortURI);
+	public void unregisterCorrelator(String uri);
+	public void registerExecutor(String uri, String inboundPortURI);
+	public String getExecutorInboundPortURI(String uri);
+	public void unregisterExecutor(String uri);
+	public void subscribe(String subscriberURI, String emitterURI);
+	public void unsubscribe(String subscriberURI, String emitterURI);
 
 }
