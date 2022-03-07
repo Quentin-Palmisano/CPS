d9236d5441df20bb478f086ea85b426891a22f8a
Quentin-Palmisano <palmisanoquentin@gmail.com>
2022-03-07 16:26:42 +0100

    test bus

---
+++ Fluctuart/src/rules/S03.java
@@ -0,0 +1,46 @@
+package rules;
+
+import java.util.ArrayList;
+
+import correlator.Health1CorrelatorStateI;
+import events.HealthEvent;
+import interfaces.CorrelatorStateI;
+import interfaces.EventBaseI;
+import interfaces.EventI;
+import interfaces.RuleI;
+
+public class S03 implements RuleI {
+
+	public S03() {
+	}
+	
+
+	@Override
+	public ArrayList<EventI> match(EventBaseI eb) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean correlate(ArrayList<EventI> matchedEvents) {
+		
+		return 	true;
+	}
+
+	@Override
+	public boolean filter(ArrayList<EventI> matchedEvents, CorrelatorStateI c) {
+		
+		return true;
+	}
+
+	@Override
+	public void act(ArrayList<EventI> matchedEvents, CorrelatorStateI c) {
+		
+	}
+
+	@Override
+	public void update(ArrayList<EventI> matchedEvents, EventBaseI eb) {
+		
+	}
+
+}
