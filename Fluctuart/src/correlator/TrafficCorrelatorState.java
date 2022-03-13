f311e708354cb5cdb9ae70a12c67515305d14b37
Paul Boursin <paul.boursin@gmail.com>
2022-03-13 23:42:41 +0100

    traffic light

---
+++ Fluctuart/src/correlator/TrafficCorrelatorState.java
@@ -0,0 +1,21 @@
+package correlator;
+
+import java.io.Serializable;
+
+import actions.TrafficAction;
+import fr.sorbonne_u.cps.smartcity.interfaces.TypeOfTrafficLightPriority;
+import interfaces.EventI;
+
+public class TrafficCorrelatorState extends CorrelatorState implements TrafficCorrelatorStateI {
+
+	@Override
+	public void receiveEvent(String emitterURI, EventI event) throws Exception {
+		
+	}
+
+	@Override
+	public void changePriority(TypeOfTrafficLightPriority priority) throws Exception {
+		executor.execute(TrafficAction.PRIORITY_CHANGE, new Serializable[] {priority});
+	}
+
+}
